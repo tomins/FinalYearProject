43789ac0ecfbed4054a930e4d6e33e8e65ce0944
Thomas Cummins <c18473984@mytudublin.ie>
2021-11-10 17:15:33 +0000

    Added users
    Added ability to add users and signUp/SignIn

--- myparkvue/src/App.vue
+++ myparkvue/src/App.vue
@@ -35,10 +35,21 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default{
   data() {
     return{
       showMobileMenu: false,//to show a menu on show a menu
+    }
+  },
+  beforeCreate(){
+    this.$store.commit('initializeStore')
+
+    const token = this.$store.state.token
+    if(token){
+      axios.defaults.headers.common['Authorization']="Token " + token
+    }else{
+      axios.defaults.headers.common['Authorization']=""
     }
   }
 }
--- myparkvue/src/router/index.js
+++ myparkvue/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import SignUp from '../views/SignUp.vue'
+import LogIn from '../views/LogIn.vue'
 
 const routes = [
   {
@@ -14,7 +16,17 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/log-in',
+    name: 'LogIn',
+    component: LogIn
+  },
+  {
+    path: '/sign-up',
+    name: 'SignUp',
+    component: SignUp
+  },
 ]
 
 const router = createRouter({
