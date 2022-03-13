51f59cf2dc386a3e11179bc3ac378b1cefe61dc9
Thomas Cummins <c18473984@mytudublin.ie>
2022-03-13 14:22:04 +0000

    refactored the python to search the database for things before trying to create it
    improved speed 10 fold

--- myparkpython/location/views.py
+++ myparkpython/location/views.py
@@ -34,32 +34,43 @@ def create(request):
     r = request.data.get('address','')
     soup= BeautifulSoup(r, 'html.parser')
     name = request.data.get('name','')
-    
-    logging.info("name: " + name)
-    address = ""
-    for x in soup.find_all('span',"street-address"):
-        address = address + x.text + ","
-    #logging.info(address)
-    lat = request.data.get('lat','')
-    long = request.data.get('long','')
-    p = []
-    for x in soup.find_all('span',"postal-code"):
-        p.append(x.text)
-    postcode = p[0]
-    postcode = postcode.split(" ",1)[0]
-    
-    #logging.info(postcode)
 
-    if name:
-        obj, location = Location.objects.get_or_create(
-            name = name,
-            lat = lat,
-            long = long,
-            address = address,
-            postcode = postcode
-        )
-        #serializer = LocationSerializer(obj, many=True)
+    try:
+        location = Location.objects.filter(
+                address__contains = name
+            )|Location.objects.filter(
+                name__contains = name
+            )
+        logging.info("the location has been found: " + name)
         return HttpResponse(status=200)
+    except Location.DoesNotExist:
+        logging.info("name: " + name)
+        address = ""
+        for x in soup.find_all('span',"street-address"):
+            address = address + x.text + ","
+        #logging.info(address)
+        lat = request.data.get('lat','')
+        long = request.data.get('long','')
+        p = []
+        for x in soup.find_all('span',"postal-code"):
+            p.append(x.text)
+        postcode = p[0]
+        postcode = postcode.split(" ",1)[0]
+        
+        #logging.info(postcode)
+
+        if name:
+            obj, location = Location.objects.get_or_create(
+                name = name,
+                lat = lat,
+                long = long,
+                address = address,
+                postcode = postcode
+            )
+            #serializer = LocationSerializer(obj, many=True)
+            return HttpResponse(status=200)
+        
+    
 
 @api_view(['POST'])
 def search(request):
@@ -123,84 +134,94 @@ def getParkingZoneQ():
             for n  in y.find_all('strong'):
                 nameA = n.get_text(strip=True) + " " #Strip means take away all whitespace at start and end of the string"
             addressA = addressA +  y.get_text(strip=True) + " " 
-            
-        postcodeA = addressA.split("London",1)[1]
-        postcodeA = postcodeA.split(" ",2)[1]
-        latlong = doLatLong(nameA)
-        #end name, address and postcode
 
-        #rates
-        ratehours = []
-        ratecost = []
-        for t in s.find_all('div',"tariff-content-row"):
-            for n in t.find_all('span',"grey"):
-                ratehours.append(n.get_text(strip = True))
-            for n in t.find_all('span',"large-text"):
-                ratecost.append(n.get_text(strip=True))
-        ratesA["hours"] = ratehours
-        ratesA["cost"] = ratecost
-        #end rates
 
-        #opening hours
-        for t in s.find_all('div',"open-hours-row"):
-            for x in t.find_all('span',"label"):
-                openingHoursA["days"] = x.get_text(strip=True)
-            for x in t.find_all('span',"entry"):
-                openingHoursA["entry"] = x.get_text(strip=True)
-            for x in t.find_all('span',"exit"):
-                openingHoursA["exit"] = x.get_text(strip=True)
-        #end opening hours
+        try:
+            parkingzone = ParkingZone.objects.filter(
+                address__contains = nameA
+            )|ParkingZone.objects.filter(
+                name__contains = nameA
+            )
+            logging.info("the location has been found: " + nameA)
+            return HttpResponse(status=200)
+        except Location.DoesNotExist:    
+            postcodeA = addressA.split("London",1)[1]
+            postcodeA = postcodeA.split(" ",2)[1]
+            latlong = doLatLong(nameA)
+            #end name, address and postcode
 
-        #services
-        for t in s.find_all('div',"row row-no-gutters services-content"):
-            for d in t.find_all('div'):
-                servicesA.append(d.get_text(strip=True))
-        #end services
+            #rates
+            ratehours = []
+            ratecost = []
+            for t in s.find_all('div',"tariff-content-row"):
+                for n in t.find_all('span',"grey"):
+                    ratehours.append(n.get_text(strip = True))
+                for n in t.find_all('span',"large-text"):
+                    ratecost.append(n.get_text(strip=True))
+            ratesA["hours"] = ratehours
+            ratesA["cost"] = ratecost
+            #end rates
 
-        #security
-        for t in servicesA:
-            if "CCTV in operation" in t:
-                securityA.append(t)
-        #end security
+            #opening hours
+            for t in s.find_all('div',"open-hours-row"):
+                for x in t.find_all('span',"label"):
+                    openingHoursA["days"] = x.get_text(strip=True)
+                for x in t.find_all('span',"entry"):
+                    openingHoursA["entry"] = x.get_text(strip=True)
+                for x in t.find_all('span',"exit"):
+                    openingHoursA["exit"] = x.get_text(strip=True)
+            #end opening hours
 
-        #spaces, disabledBays, ev, height
-        for t in s.find_all('div',"facility-card-element"):
-            for n in t.find_all(attrs={"data-notice":"Standard parking spaces"}):
-                numSpacesA = t.get_text(strip=True)
-            for n in t.find_all(attrs={"data-notice":"Blue badge parking spaces"}):
-                disabledBaysA = t.get_text(strip=True)
-            for n in t.find_all(attrs={"data-notice":"EV charging parking spaces"}):
-                evSpacesA =t.get_text(strip=True)
-            for n in t.find_all(attrs={"data-notice":"Max. height"}):
-                heightA = t.get_text(strip=True)
-        #end spaces, disabledBays, ev, height
+            #services
+            for t in s.find_all('div',"row row-no-gutters services-content"):
+                for d in t.find_all('div'):
+                    servicesA.append(d.get_text(strip=True))
+            #end services
 
-        #now to create instance of model and put into db
-        obj, park = ParkingZone.objects.get_or_create(
-            name = nameA,
-            address = addressA,
-            rates = {
-                'hours' : ratesA["hours"],
-                'price' : ratesA["cost"],
-            },
-            openingHours = {
-                'days': openingHoursA["days"],
-                'entry' : openingHoursA["entry"],
-                'exit' : openingHoursA["exit"],
-            },
-            services = {
-                'list' : [servicesA],
-            },
-            security = {
-                'list': [securityA],
-            },
-            numSpaces = numSpacesA,
-            disabledBays = disabledBaysA,
-            evSpaces = evSpacesA,
-            height = heightA,
-            postcode = postcodeA,
-            latlong = latlong
-        )
+            #security
+            for t in servicesA:
+                if "CCTV in operation" in t:
+                    securityA.append(t)
+            #end security
+
+            #spaces, disabledBays, ev, height
+            for t in s.find_all('div',"facility-card-element"):
+                for n in t.find_all(attrs={"data-notice":"Standard parking spaces"}):
+                    numSpacesA = t.get_text(strip=True)
+                for n in t.find_all(attrs={"data-notice":"Blue badge parking spaces"}):
+                    disabledBaysA = t.get_text(strip=True)
+                for n in t.find_all(attrs={"data-notice":"EV charging parking spaces"}):
+                    evSpacesA =t.get_text(strip=True)
+                for n in t.find_all(attrs={"data-notice":"Max. height"}):
+                    heightA = t.get_text(strip=True)
+            #end spaces, disabledBays, ev, height
+
+            #now to create instance of model and put into db
+            obj, park = ParkingZone.objects.get_or_create(
+                name = nameA,
+                address = addressA,
+                rates = {
+                    'hours' : ratesA["hours"],
+                    'price' : ratesA["cost"],
+                },
+                openingHours = {
+                    'days': openingHoursA["days"],
+                    'entry' : openingHoursA["entry"],
+                    'exit' : openingHoursA["exit"],
+                },
+                services = {
+                    'list' : [servicesA],
+                },
+                security = {
+                    'list': [securityA],
+                },
+                numSpaces = numSpacesA,
+                disabledBays = disabledBaysA,
+                evSpaces = evSpacesA,
+                height = heightA,
+                postcode = postcodeA,
+                latlong = latlong
+            )
         
         #end creating and saving parking zone, also end for loop for each parking area
     #after for loop through all pages ends
